82726f5d60af83490047824a6dba3ac1faf74ef9
hosenberg <khosravanianhosein2@gmail.com>
2025-04-13 15:26:02 +0330

    create App and Game

--- src/Models/App.java
+++ src/Models/App.java
@@ -2,9 +2,14 @@ package Models;
 
 import Enums.Menu;
 
+import java.util.ArrayList;
+
 public class App {
     private static Menu currentMenu = Menu.loginRegisterMenu;
-    private static Game game;
+    private static Game currentGame = null;
+    private static final ArrayList<Game> games = new ArrayList<>();
+    private static final ArrayList<GameMap> gameMaps = new ArrayList<>();
+    private static final ArrayList<Player> players = new ArrayList<>();
 
     public static Menu getCurrentMenu() {
         return currentMenu;
@@ -12,5 +17,25 @@ public class App {
 
     public static void setCurrentMenu(Menu menu) {
         currentMenu = menu;
+    }
+
+    public static Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public static void setCurrentGame(Game currentGame) {
+        App.currentGame = currentGame;
+    }
+
+    public static ArrayList<Game> getGames() {
+        return games;
+    }
+
+    public static ArrayList<GameMap> getGameMaps() {
+        return gameMaps;
+    }
+
+    public static ArrayList<Player> getPlayers() {
+        return players;
     }
 }
--- src/Models/Game.java
+++ src/Models/Game.java
@@ -1,5 +1,52 @@
 package Models;
 
-public class Game {
+import java.util.ArrayList;
 
+public class Game {
+    private static Player currentPlayer = null;
+    private static Player creatorPlayer;
+    private static ArrayList<Tile> currentMapTiles;
+    private static final DateAndTime dateAndTime = new DateAndTime();
+    private static GameMap gameMap;
+    private static final ArrayList<Player> inGamePlayers = new ArrayList<>();
+    private static final Weather weather = new Weather();
+
+    public Game(Player creatorPlayer, Player currentPlayer, ArrayList<Tile> currentMapTiles, GameMap gameMap) {
+        Game.currentPlayer = creatorPlayer;
+        Game.creatorPlayer = creatorPlayer;
+        Game.currentMapTiles = currentMapTiles;
+        Game.gameMap = gameMap;
+    }
+
+    public static Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public static void setCurrentPlayer(Player currentPlayer) {
+        Game.currentPlayer = currentPlayer;
+    }
+
+    public static Player getCreatorPlayer() {
+        return creatorPlayer;
+    }
+
+    public static void setCreatorPlayer(Player creatorPlayer) {
+        Game.creatorPlayer = creatorPlayer;
+    }
+
+    public static ArrayList<Tile> getCurrentMapTiles() {
+        return currentMapTiles;
+    }
+
+    public static void setCurrentMapTiles(ArrayList<Tile> currentMapTiles) {
+        Game.currentMapTiles = currentMapTiles;
+    }
+
+    public static GameMap getGameMap() {
+        return gameMap;
+    }
+
+    public static void setGameMap(GameMap gameMap) {
+        Game.gameMap = gameMap;
+    }
 }
