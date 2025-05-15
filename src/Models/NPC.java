7a0dcd54bd3af52c567126893447442c83cc79c0
hosenberg <khosravanianhosein2@gmail.com>
2025-05-16 02:15:17 +0330

    NPC some change

--- src/Models/NPC.java
+++ src/Models/NPC.java
@@ -3,21 +3,46 @@ package Models;
 import Models.Items.Item;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NPC {
     private final String name;
     private final ArrayList<Item> favoriteItems;
     private final ArrayList<Request> requests;
 
-    private NPC(String name, ArrayList<Item> favoriteItems, ArrayList<Request> requests) {
+    public NPC(String name, ArrayList<Item> favoriteItems, ArrayList<Request> requests) {
         this.name = name;
         this.favoriteItems = favoriteItems;
         this.requests = requests;
     }
 
-    public static NPC Sebastian("Sebastian", new ArrayList<Item>(), new ArrayList<Request>());
-    public static NPC Abigail("Abigail", new ArrayList<Item>(), new ArrayList<Request>());
-    public static NPC Harvey("Harvey", new ArrayList<Item>(), new ArrayList<Request>());
-    public static NPC Lia("Lia", new ArrayList<Item>(), new ArrayList<Request>());
-    public static NPC Robin("Robin", new ArrayList<Item>(), new ArrayList<Request>);
+    public static final NPC Sebastian = new NPC(
+            "Sebastian",
+            new ArrayList<>(Arrays.asList(/* add favorite Item objects here */)),
+            new ArrayList<>(Arrays.asList(/* add Request objects here */))
+    );
+
+    public static final NPC Abigail = new NPC(
+            "Abigail",
+            new ArrayList<>(Arrays.asList(/* add favorite Item objects here */)),
+            new ArrayList<>(Arrays.asList(/* add Request objects here */))
+    );
+
+    public static final NPC Harvey = new NPC(
+            "Harvey",
+            new ArrayList<>(Arrays.asList(/* add favorite Item objects here */)),
+            new ArrayList<>(Arrays.asList(/* add Request objects here */))
+    );
+
+    public static final NPC Lia = new NPC(
+            "Lia",
+            new ArrayList<>(Arrays.asList(/* add favorite Item objects here */)),
+            new ArrayList<>(Arrays.asList(/* add Request objects here */))
+    );
+
+    public static final NPC Robin = new NPC(
+            "Robin",
+            new ArrayList<>(Arrays.asList(/* add favorite Item objects here */)),
+            new ArrayList<>(Arrays.asList(/* add Request objects here */))
+    );
 }
