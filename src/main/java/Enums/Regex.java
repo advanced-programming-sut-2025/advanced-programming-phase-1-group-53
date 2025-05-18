ab217f8e2a551299d77e4ca6457b54638dae3d9c
hosenberg <khosravanianhosein2@gmail.com>
2025-05-18 22:59:38 +0330

    profilemenu done

--- src/main/java/Enums/Regex.java
+++ src/main/java/Enums/Regex.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public enum Regex {
     username("^^[a-zA-Z0-9-]+$$"),
-    password("[A-Za-z\\d?><\"';:\\/|}{+\\=)(*&^%$#!-]{8,}$"),
+    password("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[?><,\\\"';:\\\\/|\\\\[\\\\]{}+=)(*&^%$#!]).{8,}$"),
     email("^[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)*@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}(?![\\?<>\"';:\\/|}{+=)(*&^%$#!]).*$"),
     AT_LEAST_ONE_NUMBER(".*[0-9].*"),
     AT_LEAST_ONE_LOWERCASE(".*[a-z].*"),
@@ -16,7 +16,7 @@ public enum Regex {
     USERNAME_CONTAINS_INVALID_SYMBOLS(".*[^a-zA-Z0-9\\-].*"),
     EMAIL_USERNAME_START_END("^[a-zA-Z0-9](.*[a-zA-Z0-9])?@.+$"),
     NO_DOUBLE_DOTS("^(?!.*\\.\\.).*$"),
-    EMAIL_USERNAME_VALID("^[a-zA-Z0-9._-]+@.+$"),
+    EMAIL_USERNAME_VALID("^[a-zA-Z0-9][a-zA-Z0-9._-]*[a-zA-Z0-9]@.+$"),
     ONLY_ONE_AT_SIGN("^[^@]+@[^@]+$"),
     DOMAIN_START_END("^[^@]+@[a-zA-Z0-9].*[a-zA-Z0-9].*$"),
     DOMAIN_VALID("^[^@]+@[a-zA-Z0-9.-]+$"),
