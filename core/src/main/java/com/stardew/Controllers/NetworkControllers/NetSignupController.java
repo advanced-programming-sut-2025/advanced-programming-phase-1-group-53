352781b90b23e768e141471882922621086bb3db
sadranosaurusrex <msmohamadi1385@gmail.com>
2025-08-11 23:22:10 +0330

    adding menus for network based version

---
+++ core/src/main/java/com/stardew/Controllers/NetworkControllers/NetSignupController.java
@@ -0,0 +1,224 @@
+package com.stardew.Controllers.NetworkControllers;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.stardew.Enums.Gender;
+import com.stardew.Enums.Regex;
+import com.stardew.Models.Game.App;
+import com.stardew.Models.Game.Player;
+import com.stardew.Network.Client.ClientApp;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class NetSignupController {
+
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final String PROFILE_DIR = "profiles/";
+    private final List<Player> players = App.getInstance().getPlayers();
+    Player newPlayer;
+    Game main;
+    String message;
+    private int securityQuestionIndex = 0;
+
+    public int getSecurityQuestionIndex() {
+        return securityQuestionIndex;
+    }
+
+    public void setSecurityQuestionIndex(int securityQuestionIndex) {
+        this.securityQuestionIndex = securityQuestionIndex;
+    }
+
+    public String register(String username, String password, String confirmPassword,
+                           String nickname, String email, String gender, Game main) {
+        this.main = main;
+
+        if (!Regex.username.regexMatcher(username)) {
+            return "Invalid username. Username can only contain letters, numbers and -";
+        }
+        if (isUsernameTaken(username)) {
+            return "Username already exists. Please try again.";
+        }
+
+        if (!Regex.email.regexMatcher(email)) {
+            return "Invalid email.";
+        }
+
+        if (password.equals("accidentally") && confirmPassword.equals("accidentally")) {
+            password = generatePassword();
+        } else {
+            if (!isValidPassword(password)) {
+                return "Password must be at least 8 chars, include upper case, number, special char.";
+            }
+
+            FileHandle profileFile = Gdx.files.local(PROFILE_DIR + username + ".json");
+            if (profileFile.exists()) {
+                return "Username already exists.";
+            }
+        }
+
+
+
+        String hashedPassword = hashPassword(password);
+        newPlayer = new Player(username, nickname, hashedPassword, email, Gender.getGender(gender), ClientApp.getInstance().getConnectionThread().getClientId());
+        return finalizeRegistration(username, nickname, hashedPassword, email, Gender.getGender(gender));
+    }
+
+    private String finalizeRegistration(String username, String nickname, String hashedPassword, String email, Gender gender) {
+
+    }
+
+
+//    public String finalizeRegistration(String username, String nickname, String hashedPassword, String email, Gender gender) {
+//        FileHandle profileFile = Gdx.files.local(PROFILE_DIR + username + ".json");
+//        newPlayer = new Player(username, nickname, hashedPassword, email, gender, ClientApp.getInstance().getConnectionThread().getClientId());
+//
+//        if (profileFile.exists()) {
+//            return "Username already exists.";
+//        }
+//        if (!Gdx.files.local(PROFILE_DIR).exists()) {
+//            Gdx.files.local(PROFILE_DIR).file().mkdirs();
+//        }
+//        profileFile.writeString(gson.toJson(newPlayer.personalInfo), false);
+//        players.add(newPlayer);
+//        App.setCurrentPlayer(newPlayer);
+//        return "User data saved successfully.";
+//    }
+
+    private boolean isUsernameTaken(String username) {
+        for (Player p : players) {
+            if (p.personalInfo.getName().equalsIgnoreCase(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static class SecurityQuestion {
+        public final int index;
+        public final String question;
+        public SecurityQuestion(int index, String question) {
+            this.index = index;
+            this.question = question;
+        }
+    }
+
+    public com.stardew.Controllers.SignUpMenuController.SecurityQuestion getRandomQuestionWithIndex() {
+        List<String> questions = new ArrayList<>();
+        questions.add("9 + 0 ="); // index 0
+        questions.add("10 - 6÷2 ="); // index 1
+        questions.add("2 * 3 ="); // index 2
+        int randomIndex = new Random().nextInt(questions.size());
+        return new com.stardew.Controllers.SignUpMenuController.SecurityQuestion(randomIndex, questions.get(randomIndex));
+    }
+
+    public boolean handleQuestions(String Index, String answer, String confirmAnswer, Player newPlayer) {
+        int questionIndex;
+        int ans;
+        int confirmAns;
+        message = "";
+
+        try {
+            questionIndex = Integer.parseInt(Index);
+            ans = Integer.parseInt(answer);
+            confirmAns = Integer.parseInt(confirmAnswer);
+        } catch (NumberFormatException e) {
+            message = "Invalid question number.";
+            return false;
+        }
+        if (!answer.equals(confirmAnswer)) {
+            message = "Confirmed answer is not valid.";
+            return false;
+        }
+        else {
+            switch (questionIndex){
+                case 1:
+                    if (ans != 9) {
+                        message = "Answer is not valid.";
+                        return false;
+                    }
+                    newPlayer.personalInfo.setSecurityQuestion("9 + 0 =");
+                    newPlayer.personalInfo.setSecurityAnswer(9);
+                    break;
+                case 2:
+                    if (ans != 7) {
+                        message = "Answer is not valid.";
+                        return false;
+                    }
+                    newPlayer.personalInfo.setSecurityQuestion("10 - 6/2 =");
+                    newPlayer.personalInfo.setSecurityAnswer(7);
+                    break;
+                case 3:
+                    if (ans != 6) {
+                        message = "Answer is not valid.";
+                        return false;
+                    }
+                    newPlayer.personalInfo.setSecurityQuestion("2 * 3 =");
+                    newPlayer.personalInfo.setSecurityAnswer(6);
+                    break;
+                default:
+                    message = "Invalid question number.";
+                    return false;
+            }
+            message = Index;
+            return true;
+        }
+    }
+    public String generatePassword() {
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        String digits = "0123456789";
+        String symbols = "? > < , \" ' ; : \\/ | ] [ } { + = ) ( * & ^ % $ # !";
+        String all = upper + lower + digits + symbols;
+
+        Random rand = new Random();
+        StringBuilder password = new StringBuilder();
+        password.append(upper.charAt(rand.nextInt(upper.length())));
+        password.append(lower.charAt(rand.nextInt(lower.length())));
+        password.append(digits.charAt(rand.nextInt(digits.length())));
+        password.append(symbols.charAt(rand.nextInt(symbols.length())));
+
+        for (int i = 4; i < 9; i++) {
+            password.append(all.charAt(rand.nextInt(all.length())));
+        }
+
+        List<Character> chars = new ArrayList<>();
+        for (char c : password.toString().toCharArray()) chars.add(c);
+        Collections.shuffle(chars);
+
+        StringBuilder shuffled = new StringBuilder();
+        for (char c : chars) shuffled.append(c);
+
+        return shuffled.toString();
+    }
+
+    private boolean isValidPassword(String password) {
+        return password.length() >= 8 &&
+            password.matches(".*[A-Z].*") &&
+            password.matches(".*[0-9].*") &&
+            password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*");
+    }
+}
---
+++ core/src/main/java/com/stardew/Views/NetworkMenus/NetSignUpMenu.java
@@ -0,0 +1,168 @@
+package com.stardew.Views.NetworkMenus;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
+import com.stardew.Controllers.SignUpMenuController;
+import com.stardew.Views.AppMenu;
+import com.stardew.Views.LoginRegisterMenu;
+
+public class NetSignUpMenu extends AppMenu {
+
+    private TextField usernameField;
+    private TextField nicknameField;
+    private TextField emailField;
+    private TextField genderField;
+    private TextField passwordField;
+    private TextField confirmPasswordField;
+    private TextField securityAnswerField;
+    private Label securityQuestionLabel;
+    private Label messageLabel;
+
+    private SignUpMenuController controller;
+    private int securityQuestionIndex; // Store the index of the shown question
+
+    public NetSignUpMenu(Game main) {
+        super(main);
+        controller = new SignUpMenuController(); // Instantiate the controller
+    }
+
+    @Override
+    public void check(String scanner) {
+        // Not used for GUI
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        table.clear();
+
+        float fieldWidth = 400f;
+        float fieldHeight = 50f;
+        float buttonWidth = 300f;
+        float buttonHeight = 60f;
+        float pad = 20f;
+
+        usernameField = new TextField("", skin);
+        nicknameField = new TextField("", skin);
+        emailField = new TextField("", skin);
+        genderField = new TextField("", skin);
+        passwordField = new TextField("", skin);
+        confirmPasswordField = new TextField("", skin);
+
+        usernameField.setMessageText("Username");
+        nicknameField.setMessageText("Nickname");
+        emailField.setMessageText("Email");
+        genderField.setMessageText("Gender (MALE/FEMALE)");
+        passwordField.setMessageText("Password");
+        confirmPasswordField.setMessageText("Confirm Password");
+
+        passwordField.setPasswordMode(true);
+        passwordField.setPasswordCharacter('*');
+        confirmPasswordField.setPasswordMode(true);
+        confirmPasswordField.setPasswordCharacter('*');
+
+        TextButton signupBtn = new TextButton("Sign Up", skin);
+        TextButton backBtn = new TextButton("Back", skin);
+
+
+        // Get a random question and its index from the controller
+        SignUpMenuController.SecurityQuestion securityQuestion = controller.getRandomQuestionWithIndex();
+        securityQuestionIndex = securityQuestion.index;
+        securityQuestionLabel = new Label(securityQuestion.question, skin);
+        securityAnswerField = new TextField("", skin);
+        securityAnswerField.setMessageText("Answer");
+
+
+        messageLabel = new Label("", skin);
+        messageLabel.setAlignment(Align.center);
+        messageLabel.setWrap(true);
+
+
+        signupBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                handleSignup();
+            }
+        });
+
+        backBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                main.setScreen(new LoginRegisterMenu(main));
+            }
+        });
+
+        // Layout
+        table.add(usernameField).width(fieldWidth).height(fieldHeight).pad(pad).row();
+        table.add(nicknameField).width(fieldWidth).height(fieldHeight).pad(pad).row();
+        table.add(emailField).width(fieldWidth).height(fieldHeight).pad(pad).row();
+        table.add(genderField).width(fieldWidth).height(fieldHeight).pad(pad).row();
+        table.add(passwordField).width(fieldWidth).height(fieldHeight).pad(pad).row();
+        table.add(confirmPasswordField).width(fieldWidth).height(fieldHeight).pad(pad).row();
+        table.add(securityQuestionLabel).width(fieldWidth).pad(pad).row();
+        table.add(securityAnswerField).width(fieldWidth).height(fieldHeight).pad(pad).row();
+        table.add(signupBtn).width(buttonWidth).height(buttonHeight).pad(pad).row();
+        table.add(backBtn).width(buttonWidth).height(buttonHeight).pad(pad).row();
+        table.add(messageLabel).width(fieldWidth).pad(pad).row();
+    }
+
+    private void handleSignup() {
+        String username = usernameField.getText().trim();
+        String nickname = nicknameField.getText().trim();
+        String email = emailField.getText().trim();
+        String gender = genderField.getText().trim().toUpperCase();
+        String password = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
+        String answer = securityAnswerField.getText().trim();
+
+        if (username.isEmpty() || nickname.isEmpty() || email.isEmpty() ||
+            gender.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || answer.isEmpty()) {
+            messageLabel.setText("Please fill in all fields.");
+            return;
+        }
+
+        // Check the answer for the security question
+        boolean answerCorrect = false;
+        controller.setSecurityQuestionIndex(securityQuestionIndex);
+        switch (controller.getSecurityQuestionIndex()) {
+            case 0:
+                answerCorrect = answer.equals("9");
+                break;
+            case 1:
+                answerCorrect = answer.equals("7");
+                break;
+            case 2:
+                answerCorrect = answer.equals("6");
+                break;
+        }
+        if (!answerCorrect) {
+            messageLabel.setText("Incorrect answer to the security question.");
+            return;
+        }
+
+        // Pass values to controller
+        String message = controller.register(username, password, confirmPassword, nickname, email, gender, main);
+
+        System.out.println(message);
+        if (!message.isEmpty()) {
+            messageLabel.setText(message);
+            return;
+        }
+        // You could inspect controller state if needed, or redirect immediately
+//        messageLabel.setText("Account created. Set security question next.");
+//        usernameField.setText("");
+//        nicknameField.setText("");
+//        emailField.setText("");
+//        genderField.setText("");
+//        passwordField.setText("");
+//        confirmPasswordField.setText("");
+
+        // Optionally go to question screen or login screen
+        // main.setScreen(new QuestionMenu(main, controller.getPlayer()));
+    }
+}
